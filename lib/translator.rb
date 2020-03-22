5ab4087337dbfea7bd0501044e25e76190c6f91d
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-22 19:23:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 
   
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
- 
+ library = load_library(file_path)
   if library["get_meaning"].include?(japanese_emoticon) 
     library["get_meaning"][japanese_emoticon]
   else
