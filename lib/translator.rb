e79c2475946e826f3cd04f763c24c3199432047b
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-22 19:21:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,9 +20,15 @@ end
 
 
 
-def get_english_meaning
+
   
-  
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+ 
+  if library["get_meaning"].include?(japanese_emoticon) 
+    library["get_meaning"][japanese_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
   
   
   # code goes here
